891b0a1ffb95293be43be975fa69708786024676
abhaysurya10 <abhaysurya10@gmail.com>
2023-10-23 21:46:36 +0530

    Add project files.

---
+++ RetrieveStudData/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RetrieveStudData
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                FileStream fs = new FileStream(@"C:\Users\ADMIN\source\repos\RetrieveStudData\StudData\StudDataFile.txt", FileMode.Open, FileAccess.Read);
+                StreamReader sr = new StreamReader(fs);
+                try
+                {
+                    string file_data = sr.ReadToEnd();
+                    Console.WriteLine(file_data);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                finally
+                {
+                    sr.Close();
+                    fs.Close();
+                    sr.Dispose();
+                    fs.Dispose();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadLine();
+        }
+    }
+}
